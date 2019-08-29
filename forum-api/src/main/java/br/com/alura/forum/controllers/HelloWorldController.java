c738af9022489bc7cd6b5d8fe1d295cdd8100179
FAZENDA\tnaraujo <tnaraujo@fazenda.ms.gov.br>
2019-08-29 17:22:14 -0400

    #1: Projeto Inicial

---
+++ forum-api/src/main/java/br/com/alura/forum/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package br.com.alura.forum.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @ResponseBody
+    @RequestMapping(path = "/")
+    public String heloWold(){
+        return "Hello World Api Forum testeteste";
+    }
+}
---
+++ forum-api/src/main/java/br/com/alura/forum/model/Topico.java
@@ -0,0 +1,52 @@
+package br.com.alura.forum.model;
+
+import br.com.alura.forum.enumaration.StatusTopico;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Topico {
+
+	private Long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime dataCriacao = LocalDateTime.now();
+	private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
+	private Usuario autor;
+	private Curso curso;
+	private List<Resposta> respostas = new ArrayList<>();
+
+	public Topico(String titulo, String mensagem, Curso curso) {
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Topico other = (Topico) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
