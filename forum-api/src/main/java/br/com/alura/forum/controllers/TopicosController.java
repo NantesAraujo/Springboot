dcd638efc2c8c16f0ca08bd553c72f7727bbc312
thiagoNunes <thiago-nunes-16@hotmail.com>
2019-09-18 23:49:36 -0300

    #9: Adicionando Especificacao Bean Validation

--- forum-api/src/main/java/br/com/alura/forum/controllers/TopicosController.java
+++ forum-api/src/main/java/br/com/alura/forum/controllers/TopicosController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -41,7 +42,7 @@ public class TopicosController {
     }
 
     @PostMapping
-    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoCadastroDto topicoCadastroDto, UriComponentsBuilder uriBuilder){
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoCadastroDto topicoCadastroDto, UriComponentsBuilder uriBuilder){
         Topico topico = topicoCadastroDto.converter(cursoRepository);
         topicoRepository.save(topico);
 
--- forum-api/src/main/java/br/com/alura/forum/dto/TopicoCadastroDto.java
+++ forum-api/src/main/java/br/com/alura/forum/dto/TopicoCadastroDto.java
@@ -4,14 +4,21 @@ import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class TopicoCadastroDto {
 
+    @NotEmpty @NotNull @Length(min = 5)
     private String titulo;
 
+    @NotEmpty @NotNull
     private String mensagem;
 
+    @NotEmpty @NotNull
     private String nomeDoCurso;
 
     public Topico converter(CursoRepository cursoRepository){
